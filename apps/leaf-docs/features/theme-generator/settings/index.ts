788de3c92ac247a44beb60ff78d84c7bb0534d17
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-03-24 16:12:42 -0700

    feat: support for tailwind 50 and 950 color stops

--- apps/leaf-docs/features/theme-generator/settings/index.ts
+++ apps/leaf-docs/features/theme-generator/settings/index.ts
@@ -1,5 +1,6 @@
 export const HEX_LENGTH = 7
 export const colorData = { key: '', label: '', hex: '', rgb: '', on: '' } as const
+import { colorNames, stopsWBase } from '../../../settings/global'
 
 export interface ColorData {
   key: string
@@ -9,82 +10,11 @@ export interface ColorData {
   on: string
 }
 
-export const steppedSwatchColorClasses: Record<string, Record<string, string>> = {
-  primary: {
-    '100': 'bg-primary-100',
-    '200': 'bg-primary-200',
-    '300': 'bg-primary-300',
-    '400': 'bg-primary-400',
-    base: 'bg-primary',
-    '600': 'bg-primary-600',
-    '700': 'bg-primary-700',
-    '800': 'bg-primary-800',
-    '900': 'bg-primary-900',
-  },
-  secondary: {
-    '100': 'bg-secondary-100',
-    '200': 'bg-secondary-200',
-    '300': 'bg-secondary-300',
-    '400': 'bg-secondary-400',
-    base: 'bg-secondary',
-    '600': 'bg-secondary-600',
-    '700': 'bg-secondary-700',
-    '800': 'bg-secondary-800',
-    '900': 'bg-secondary-900',
-  },
-  tertiary: {
-    '100': 'bg-tertiary-100',
-    '200': 'bg-tertiary-200',
-    '300': 'bg-tertiary-300',
-    '400': 'bg-tertiary-400',
-    base: 'bg-tertiary',
-    '600': 'bg-tertiary-600',
-    '700': 'bg-tertiary-700',
-    '800': 'bg-tertiary-800',
-    '900': 'bg-tertiary-900',
-  },
-  success: {
-    '100': 'bg-success-100',
-    '200': 'bg-success-200',
-    '300': 'bg-success-300',
-    '400': 'bg-success-400',
-    base: 'bg-success',
-    '600': 'bg-success-600',
-    '700': 'bg-success-700',
-    '800': 'bg-success-800',
-    '900': 'bg-success-900',
-  },
-  warning: {
-    '100': 'bg-warning-100',
-    '200': 'bg-warning-200',
-    '300': 'bg-warning-300',
-    '400': 'bg-warning-400',
-    base: 'bg-warning',
-    '600': 'bg-warning-600',
-    '700': 'bg-warning-700',
-    '800': 'bg-warning-800',
-    '900': 'bg-warning-900',
-  },
-  error: {
-    '100': 'bg-error-100',
-    '200': 'bg-error-200',
-    '300': 'bg-error-300',
-    '400': 'bg-error-400',
-    base: 'bg-error',
-    '600': 'bg-error-600',
-    '700': 'bg-error-700',
-    '800': 'bg-error-800',
-    '900': 'bg-error-900',
-  },
-  neutral: {
-    '100': 'bg-neutral-100',
-    '200': 'bg-neutral-200',
-    '300': 'bg-neutral-300',
-    '400': 'bg-neutral-400',
-    base: 'bg-neutral',
-    '600': 'bg-neutral-600',
-    '700': 'bg-neutral-700',
-    '800': 'bg-neutral-800',
-    '900': 'bg-neutral-900',
-  },
-}
+export const steppedSwatchColorClasses: Record<string, Record<string, string>> = {}
+
+colorNames.forEach((colorName) => {
+  steppedSwatchColorClasses[colorName] = {}
+  stopsWBase.forEach((stop) => {
+    steppedSwatchColorClasses[colorName][stop] = stop === 'base' ? `bg-${colorName}` : `bg-${colorName}-${stop}`
+  })
+})
--- apps/leaf-docs/settings/global.ts
+++ apps/leaf-docs/settings/global.ts
@@ -9,9 +9,9 @@ export const colorNames = [
   'neutral',
 ] as const
 
-export const stops = ['100', '200', '300', '400', '600', '700', '800', '900'] as const
+export const stops = ['50', '100', '200', '300', '400', '600', '700', '800', '900', '950'] as const
 
-export const stopsWBase = [...stops.slice(0, 4), 'base', ...stops.slice(4)] as const
+export const stopsWBase = [...stops.slice(0, 5), 'base', ...stops.slice(5)] as const
 
 export type ColorNames = (typeof colorNames)[number]
 export type Stops = (typeof stops)[number]
