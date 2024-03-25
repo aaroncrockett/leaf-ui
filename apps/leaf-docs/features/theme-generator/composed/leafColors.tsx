1f634d72e6095a6422c78d7e515f688029b0a7b5
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-03-24 18:07:14 -0700

    chore: rename func for clarity

--- apps/leaf-docs/features/theme-generator/composed/leafColors.tsx
+++ apps/leaf-docs/features/theme-generator/composed/leafColors.tsx
@@ -23,16 +23,16 @@ export const generateRandomHex = (lib: any) => () => {
   }
 }
 
-export const generateRotatedHue = (lib: any) => (color: string, by: string) => {
+export const generateRotatedHueHex = (lib: any) => (color: string, by: string) => {
   return chroma(color).set('hsl.h', by).hex()
 
   console.warn('You passed in an unsupported type.')
 }
 
-export const generateTriadColors = (lib: any) => (generateRotatedHue: any) => (color: string) => {
+export const generateTriadColors = (lib: any) => (generateRotatedHueHex: any) => (color: string) => {
   try {
-    const color2 = generateRotatedHue(color, '+120')
-    const color3 = generateRotatedHue(color, '-120')
+    const color2 = generateRotatedHueHex(color, '+120')
+    const color3 = generateRotatedHueHex(color, '-120')
     return { result: [color2, color3], success: true }
   } catch (e) {
     console.error(e)
@@ -40,17 +40,17 @@ export const generateTriadColors = (lib: any) => (generateRotatedHue: any) => (c
   }
 }
 
-export const depFuncs = [generateRandomHex, generateRotatedHue, generateTriadColors]
+export const depFuncs = [generateRandomHex, generateRotatedHueHex, generateTriadColors]
 
 export const createLeafColors = (reduceFuncs: any) => (lib: any, depFuncs: any[]) => {
   const funcs = reduceFuncs(depFuncs, [lib])
 
   // funcs dep on other color funcs
-  const generateTriadColors = funcs.generateTriadColors(funcs.generateRotatedHue)
+  const generateTriadColors = funcs.generateTriadColors(funcs.generateRotatedHueHex)
 
   return {
     generateRandomHex: funcs.generateRandomHex,
-    generateRotatedHue: funcs.generateRotatedHue,
+    generateRotatedHueHex: funcs.generateRotatedHueHex,
     generateTriadColors: generateTriadColors,
   }
 }
