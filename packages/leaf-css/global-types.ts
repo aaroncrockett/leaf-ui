e42b58c688b0d710447a3a47f8a366b1eae56e12
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-03-26 08:10:25 -0700

    chore: refine global types locations

--- packages/leaf-css/global-types.ts
+++ packages/leaf-css/global-types.ts
@@ -1,19 +1,3 @@
-import { colorNames, stops } from './settings'
-
-export interface ThemeInfo {
-  theme: Theme
-  name: string
-}
-
-export interface Theme {
-  colors: { [key: string]: string }
-  general: { [key: string]: string }
-  name: string
-}
-
-export type LeafColorName = typeof colorNames
-export type LeafColorNames = (typeof colorNames)[number]
-
 export interface GenericStringValueObject {
   [key: string]: Record<string, string>
   // Define other properties if needed
